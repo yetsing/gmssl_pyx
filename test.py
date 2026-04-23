3abcce6839f3587a33ecc6e2a025bf4701cdbe8d
yetsing <heopro@163.com>
2026-04-23 08:45:14 +0800

    refactor: enhance test execution by allowing repeat runs based on environment variable

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
-import unittest
+import os
 import sys
+import unittest
+
 
 def main():
     loader = unittest.defaultTestLoader
-    suite = loader.discover("tests")
-    runner = unittest.TextTestRunner(verbosity=2)
-    result = runner.run(suite)
-    # If there were any failures or errors, exit 1
-    if not result.wasSuccessful():
-        sys.exit(1)
+    repeat = int(os.environ.get("TEST_REPEAT", "1"))
+    for _ in range(repeat):
+        suite = loader.discover("tests")
+        runner = unittest.TextTestRunner(verbosity=1)
+        result = runner.run(suite)
+        # If there were any failures or errors, exit 1
+        if not result.wasSuccessful():
+            sys.exit(1)
+
 
 if __name__ == "__main__":
     main()
